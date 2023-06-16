8224f46d0a375007f9f33615a4b29c9ef6b4c2da
CinCariddi <cintiacariddi@gmail.com>
2023-06-16 19:30:44 -0300

    Ruta get parseHub

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,4 +1,8 @@
 const env = {
-	port: process.env.PORT || 3000
+	port: process.env.PORT || 3000,
+	api_key: process.env.API_KEY,
+	promart_token_ladrillos: process.env.PROMART_TOKEN_LADRILLOS,
+	promart_token_pintura: process.env.PROMART_TOKEN_PINTURA,
+	promart_token_cemento: process.env.PROMART_TOKEN_CEMENTO,
 }
 export default env
